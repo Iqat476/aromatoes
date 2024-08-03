acc465a061830b305b3df202921b22f05b183183
Muhammad <iqat476@gmail.com>
2024-08-03 12:36:31 +0400

    fix: make footer text white

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<div className="py-4 bg-zinc-800 flex justify-around border-gray-500 border-t-2">
+		<div className="py-4 bg-zinc-800 flex justify-around border-gray-500 border-t-2 text-light">
 			<h4>Owned by Susu, and Ducky</h4>
 			<h4>All rights reserved©</h4>
 			<h4>Made by Iqat</h4>
